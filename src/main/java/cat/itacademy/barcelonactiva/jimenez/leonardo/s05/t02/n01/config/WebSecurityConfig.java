b5911119b1bad18b6bcdddf9272ab47456814dee
Leonardo Jimenez <ljimenezubillus@gmail.com>
2022-10-19 22:37:08 +0200

    fixing date and fixing swagger access without security

--- src/main/java/cat/itacademy/barcelonactiva/jimenez/leonardo/s05/t02/n01/config/WebSecurityConfig.java
+++ src/main/java/cat/itacademy/barcelonactiva/jimenez/leonardo/s05/t02/n01/config/WebSecurityConfig.java
@@ -50,7 +50,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		// We don't need CSRF for this example
 		httpSecurity.csrf().disable()
 				// dont authenticate this particular request
-				.authorizeRequests().antMatchers("/authenticate").permitAll().
+				.authorizeRequests().antMatchers("/authenticate","/v2/api-docs","/favicon.ico"
+						,"/swagger-ui/**","/swagger-resources/**").permitAll().
 				// all other requests need to be authenticated
 				anyRequest().authenticated().and().
 				// make sure we use stateless session; session won't be used to
